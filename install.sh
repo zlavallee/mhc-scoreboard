31fe08dad7cc2c9dd6a69e9c064f0b7e05de9f63
Zack LaVallee <zlavallee@gmail.com>
2022-05-17 08:05:44 -0500

    Adding service

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+chmod +x ./run.sh
+chmod +x ./mhc-scoreboard.service
 
 python3.9 -m venv ~/.venvs/mhc-scoreboard
 source ~/.venvs/mhc-scoreboard/bin/activate
 pip install -r requirements.txt
 pip install RPi.GPIO
+
+cp ./mhc-scoreboard.service /etc/systemd/system/mhc-scoreboard.service
